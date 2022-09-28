7b1953c6d25c8b357670638d5234fc72cd8a2440
Gladys Miguel Felipe <l19350306@tuxtepec.tecnm.mx>
2022-09-28 18:08:17 -0500

    Actoalizacion

--- controllers/messages.js
+++ controllers/messages.js
@@ -1,14 +1,29 @@
-const rootMessage = (req, res) => {
-    res.send('Mesajes')
-}
+const { response } = require("express")
 
-const hiMessage = (req, res) => {
-    res.send('Hola Mundo')
+const rootMessage = (req = request, res = response) => {
+    res.status(404).json({msg: 'Mesajes'}) 
 
 }
 
-const byeMessage = (req, res) =>{
-    res.send('Adios Mundo')
+const hiMessage = (req = resquest, res= response)  => {
+    res.status(300).json({msg:'Hola Mundo'})
+
 }
 
-module.exports = {rootMessage, hiMessage, byeMessage}
+const byeMessage = (req = resquest, res= response) =>{
+    res.status(500).json({msg:'Adios Mundo'})
+}
+
+const postMessage =(req = resquest, res= response) => {
+    res.status(209).json({msg:'Mensaje post'})
+}
+
+const putMessage =(req = resquest, res= response) => {
+    res.status(105).json({msg:'Mensaje PUT'})
+}
+
+const deleteMessage =(req = resquest, res= response) => {
+    res.status(401).json({msg:'Mensaje DELETE'})
+}
+
+module.exports = {rootMessage, hiMessage, byeMessage, postMessage, putMessage, deleteMessage}
--- routes/messages.js
+++ routes/messages.js
@@ -1,13 +1,22 @@
-//http://localhost:4000/api/v1/messages/
-
 const {Router} = require('express')
 const router = Router()
-const {rootMessage, hiMessage, byeMessage} = require('../controllers/messages')
 
+const {
+    rootMessage,
+    hiMessage,
+    byeMessage,
+    postMessage,
+    putMessage,
+    deleteMessage,
+}
+     = require('../controllers/messages')
+     //router.get("", () => {})
     router.get('/', rootMessage)//End Point
-
     router.get('/hi', hiMessage)//End Point
-
     router.get('/bye', byeMessage)//End Point
+
+    router.post('/', postMessage)//End Point
+    router.put('/', putMessage)//End Point
+    router.delete('/', deleteMessage)//End Point
 
     module.exports = router
